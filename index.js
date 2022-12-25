2b4258457735732577a49c4f2a6952884f471e8f
Jaskejaske1 <jasper.savels@gmail.com>
2022-12-25 22:33:02 +0100

    Main file setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import inquirer from 'inquirer';
 import * as TOML from '@ltd/j-toml';
 import fs from 'fs';
+
+// TODO: Add a way to get the path to the wanted file from the user via CLI (inquirer)
