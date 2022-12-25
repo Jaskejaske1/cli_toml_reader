aa8ea21bda1507d0be1a786ac26b86c84da1041d
Jaskejaske1 <jasper.savels@gmail.com>
2022-12-25 22:32:11 +0100

    Directory setup

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 import inquirer from 'inquirer';
+import * as TOML from '@ltd/j-toml';
 import fs from 'fs';
-
-// create a list of all the subjects from the folders inside the subjects folder
-const subjects = fs.readdirSync('./subjects');
-
-// log the subjects to the console (json stringified)
-console.log(JSON.stringify(subjects, null, 2));
