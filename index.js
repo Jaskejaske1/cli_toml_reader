a934a3d5a3270ebc648ffa8c990f5d8d20d0d344
Jaskejaske1 <jasper.savels@gmail.com>
2022-12-28 20:58:29 +0100

    Search system update

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ const selectFileOrFolder = (directory) => {
 				return;
 			}
 
+			// Add the "Go to parent folder" option to the list of choices
+			const choices = [...entries, 'Go to parent folder'];
+
 			inquirer.prompt([
 				{
 					type: 'list',
 					name: 'selection',
 					message: 'Select a file or folder:',
-					choices: entries,
+					choices: choices,
 				},
 			]).then(resolve);
 		});
@@ -33,31 +36,37 @@ const selectTomlFile = async (directory) => {
 		// Display the list of entries to the user
 		const { selection } = await selectFileOrFolder(directory);
 
-		// Get the full path to the selected entry
-		const fullPath = path.join(directory, selection);
-
-		// Check if the selected entry is a file or a folder
-		const stat = await new Promise((resolve, reject) => {
-			fs.stat(fullPath, (err, stat) => {
-				if (err) {
-					reject(err);
-					return;
-				}
-
-				resolve(stat);
-			});
-		});
-
-		if (stat.isDirectory()) {
-			// If the selected entry is a folder, call the selectTomlFile function again with the path to the selected folder
-			return selectTomlFile(fullPath);
-		} else if (path.extname(selection) === '.toml') {
-			// If the selected entry is a .toml file, return the path to the file
-			return fullPath;
+		if (selection === 'Go to parent folder') {
+			// If the user has selected the "Go to parent folder" option, get the parent folder's path and call the selectTomlFile function again with the parent folder's path
+			const parentFolder = path.dirname(directory);
+			return selectTomlFile(parentFolder);
 		} else {
-			// If the selected entry is not a .toml file, display an error message and prompt the user to select a different file or folder
-			console.error('Please select a .toml file.');
-			return selectTomlFile(directory);
+			// Get the full path to the selected entry
+			const fullPath = path.join(directory, selection);
+
+			// Check if the selected entry is a file or a folder
+			const stat = await new Promise((resolve, reject) => {
+				fs.stat(fullPath, (err, stat) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+
+					resolve(stat);
+				});
+			});
+
+			if (stat.isDirectory()) {
+				// If the selected entry is a folder, call the selectTomlFile function again with the path to the selected folder
+				return selectTomlFile(fullPath);
+			} else if (path.extname(selection) === '.toml') {
+				// If the selected entry is a .toml file, return the path to the file
+				return fullPath;
+			} else {
+				// If the selected entry is not a .toml file, display an error message and prompt the user to select a different file or folder
+				console.error('Please select a .toml file.');
+				return selectTomlFile(directory);
+			}
 		}
 	} catch (err) {
 		console.error(err);
@@ -65,6 +74,7 @@ const selectTomlFile = async (directory) => {
 	}
 };
 
+// The path to the TOML file
 const filePath = await selectTomlFile(subjectsFolder);
 
 // Read the TOML file
@@ -82,5 +92,49 @@ const toml = await new Promise((resolve, reject) => {
 // Parse the TOML file
 const parsedToml = TOML.parse(toml);
 
-// Display the parsed TOML file
-console.log(parsedToml);
+// Ask the user for a definition
+const askForDefinition = async () => {
+	try {
+		const { definition } = await inquirer.prompt([
+			{
+				type: 'input',
+				name: 'definition',
+				message: 'Enter a definition:',
+			},
+		]);
+
+		// Check if the entered definition exists in the parsed TOML file
+		if (parsedToml[definition]) {
+			// If the entered definition exists, display its content
+			console.log(parsedToml[definition]);
+		} else {
+			// If the entered definition does not exist, display an error message
+			console.error('Definition not found.');
+		}
+
+		// Ask the user if they want to ask another question
+		const { askAgain } = await inquirer.prompt([
+			{
+				type: 'confirm',
+				name: 'askAgain',
+				message: 'Do you want to ask another question?',
+				default: true,
+			},
+		]);
+
+		if (askAgain) {
+			// If the user wants to ask another question, call the askForDefinition function again
+			return askForDefinition();
+		} else {
+			// If the user doesn't want to ask another question, exit the program
+			console.log('Goodbye!');
+			process.exit(0);
+		}
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+};
+
+// Call the askForDefinition function
+askForDefinition();
